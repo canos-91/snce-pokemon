0e39a8f1d947198db9f406586e45bc41e1c1f449
Marco <marco.canopoli@digitiamo.com>
2023-04-02 18:16:38 +0200

    prisma migrations

--- app/src/pages/api/hello.ts
+++ app/src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
 
-type Data = {
-  name: string
-}
+const prisma: PrismaClient = new PrismaClient()
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const trainer = await prisma.trainer.findMany()
+  console.log(trainer)
+  res.status(200).json({ trainer })
 }
